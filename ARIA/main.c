4df6917a715d3475f029790f5c330050596bd2b0
GunwooYun <gunwoo@gmail.com>
2022-11-07 10:56:18 +0900

    ARIA crypto init version

---
+++ ARIA/main.c
@@ -0,0 +1,19 @@
+/**************
+Author : Gunwoo Yun
+Date : 22.11.07
+Crypto : ARIA
+**************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/aria.h>
+
+typedef unsigned char U1;
+
+int main(void)
+{
+	U1 user_key[] = {};
+	//U1 plain_txt[] = {
+	return 0;
+}
