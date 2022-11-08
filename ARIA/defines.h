947d41b35648263ca444828f839efb14a9f3ddf1
GunwooYun <gunwoo87@gmail.com>
2022-11-08 09:54:24 +0900

    Add defines.h header file

---
+++ ARIA/defines.h
@@ -0,0 +1,18 @@
+/**************
+Author : Gunwoo Yun
+Date : 22.11.08
+Crypto : ARIA
+**************/
+
+#ifndef __DEFINES_H_
+#define __DEFINES_H_
+
+typedef unsigned char   U1;
+typedef unsigned short  U2;
+typedef unsigned int    U4;
+
+#define SUCCESS 0x9000
+#define IN
+#define OUT
+
+#endif
