be83fb0c7078e52646dfb6f79c57084ea1cf7b75
GunwooYun <gunwoo87@gmail.com>
2022-11-09 22:39:03 +0900

    Remove hmac codes in main.c

--- OpenSSLtool/main.c
+++ OpenSSLtool/main.c
@@ -17,48 +17,8 @@ Crypto : ARIA HMAC
 EVP_CIPHER_CTX *evp_ctx_enc = NULL;
 EVP_CIPHER_CTX *evp_ctx_dec = NULL;
 
-//HMAC_CTX *hmac_ctx = NULL;
 
 U1 cipher_type[12];
-
-#if 0
-U2 HMAC_init(IN U1 *key, IN U4 key_len)
-{
-	int ret = 0;
-
-	hmac_ctx = HMAC_CTX_new();
-	if(hmac_ctx == NULL){
-		printf("HMAC_CTX_new() is NULL\n");
-		return 1;
-	}
-
-	ret = HMAC_Init_ex(hmac_ctx, key, (int)key_len, EVP_sha256(), NULL);
-	if(!ret){
-		printf("HMAC_Init_ex failed\n");
-		return 1;
-	}
-	return 0x9000;
-}
-
-U2 HMAC_update(IN U1 *data, IN U4 data_len)
-{
-	int ret = 0;
-	
-	ret = HMAC_Update(hmac_ctx, data, (size_t)data_len);
-	printf("update ret : %d\n", ret);
-
-	return 0x9000;
-}
-
-U2 HMAC_final(OUT U1 *md, OUT U4 *md_len)
-{
-	int ret = 0;
-	ret = HMAC_Final(hmac_ctx, md, md_len);
-	printf("final ret : %d\n", ret);
-
-	return 0x9000;
-}
-#endif
 
 U2 ARIA_Enc_Init(IN U1 *key, IN U1 block_mode, IN U2 iv_len, IN U1 *iv)
 {
