1ebb1d596532b12300be4a08a18fc0abf3557201
GunwooYun <gunwoo87@gmail.com>
2022-11-16 17:13:36 +0900

    Make Enc Dec for ARIA

--- OpenSSLtool/main.c
+++ OpenSSLtool/main.c
@@ -1,6 +1,6 @@
 /*******************
 Author : Gunwoo Yun
-Date : 22.11.14
+Date : 22.11.16
 Crypto : ARIA HMAC
 *******************/
 
@@ -14,106 +14,67 @@ Crypto : ARIA HMAC
 #include "./inc/hmacc.h"
 #include "./inc/aria.h"
 
-EVP_CIPHER_CTX *evp_ctx_enc_gcm = NULL;
-EVP_CIPHER_CTX *evp_ctx_dec_gcm = NULL;
-U1 cipher_type_gcm[12];
-
 int main(void)
 {
 	U2 ret = 0;
-	U1 key_short[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+	U1 key[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
 	//U1 key_hmac[16] = {0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,0x69,0x6a,0x6b,0x6c,0x6d,0x6e,0x6f,0x70};
-	U1 key_hmac[16] = {"abcdefghijklmnop"};
+	//U1 key_hmac[16] = {"abcdefghijklmnop"};
 	U1 iv[] = { 0x0f, 0x02, 0x05, 0x03, 0x08, 0x05, 0x07, 0xaa, 0xbb, 0xcc, 0xda, 0xfb, 0xcc, 0xd0, 0xe0, 0xf0 }; // 16bytes
-	U1 AAD[] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };	// 16 Bytes
+	U1 aad[] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };	// 16 Bytes
 
-	U1 plain_text_short[128] = {0x00, };
-	U1 plain_text_long[512] = {0x00, };
-	U1 plain_text_longlong[1024] = {0x00, };
-
-	U1 cipher_text[3400] = {0x00, };
-	U4 cipher_len = 0;
-
-	U1 plain_text[3400] = {0x00, };
+	U1 plain[127] = {0x00, };
 	U4 plain_len = 0;
 
-	U1 msgDgst[3400] = {0x00, };
-	U4 msgDgst_len = 0;
-	U1 hmacData[30] = {"hello,world"};
+	U1 cipher[3000] = {0x00, };
+	U4 cipher_len = 0;
 
 	U4 req_tag_len = 14;
 
 	U1 tag[17] = {0x00, };
 	U4 tag_len = 0;
 
-	U1 aad[] = { 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF };	// 16 Bytes
+	/* Plain Text get random value */
+	ret = RAND_bytes(plain, 127);
+	if(!ret)
+    {
+        printf("Random Plain Text Failed\n");
+		return -1;
+    }
 
-	ret = RAND_bytes(plain_text_short, 128);
-	if(!ret)
-    {
-        printf("RAND_bytes ERROR\n");
-		return 1;
-    }
-	ret = RAND_bytes(plain_text_long, 512);
-	if(!ret)
-    {
-        printf("RAND_bytes ERROR\n");
-		return -1;
-    }
-	ret = RAND_bytes(plain_text_longlong, 1024);
-	if(!ret)
-    {
-        printf("RAND_bytes ERROR\n");
-		return -1;
-    }
+	//U1 msgDgst[3400] = {0x00, };
+	//U4 msgDgst_len = 0;
+	//U1 hmacData[30] = {"hello,world"};
+
+
+	printf("******* Key ************\n");
+	for(int i = 0; i < sizeof(key); i++)
+		printf("%#x ", key[i]);
+	printf("\n");
 
 	printf("******* ARIA Encryption Start ************\n");
 	printf("Plain Text\n");
-	for(int i = 0; i < sizeof(plain_text_short); i++)
-		printf("%#x ", plain_text_short[i]);
+	for(int i = 0; i < sizeof(plain); i++)
+		printf("%#x ", plain[i]);
 	printf("\n");
-	ret = ARIA_Enc_Init(key_short, MODE_GCM, sizeof(iv), iv, sizeof(aad), aad);
-	if(ret != SUCCESS)
-	{
-		printf("aria gcm enc init failed\n");
-		return 1;
-	}
 
-	ret = ARIA_Enc_Update(PADDING_BLOCK, MODE_GCM, plain_text_short, sizeof(plain_text_short), cipher_text, &cipher_len, req_tag_len, tag, &tag_len);
-	if(ret != SUCCESS)
-	{
-		printf("aria gcm enc update failed\n");
-		return 1;
-	}
+	ret = EncryptARIA(key, PADDING_BLOCK, MODE_ECB, plain, sizeof(plain), cipher, &cipher_len, req_tag_len, tag, &tag_len, sizeof(iv), iv, sizeof(aad), aad);
+	printf("cipher length : %d\n", cipher_len);
+
 	printf("Cipher Text\n");
 	for(int i = 0; i < cipher_len; i++)
-		printf("%#x ", cipher_text[i]);
+		printf("%#x ", cipher[i]);
 	printf("\n");
 
-	//cipher_text[0] = 0xff;
+	printf("******* ARIA Decryption Start ************\n");
 
-	printf("******* ARIA GCM Decryption Start ************\n");
-	printf("Cipher Text\n");
-	for(int i = 0; i < cipher_len; i++)
-		printf("%#x ", cipher_text[i]);
-	printf("\n");
+	memset(plain, 0, sizeof(plain)); // plain buffer init 0
 
-	ret = ARIA_Dec_Init(key_short, MODE_GCM, sizeof(iv), iv, sizeof(aad), aad);
-	if(ret != SUCCESS)
-	{
-		printf("aria gcm dec init failed\n");
-		return 1;
-	}
+	ret =  DecryptARIA(key, PADDING_BLOCK, MODE_ECB, cipher, cipher_len, plain, &plain_len, tag, tag_len, sizeof(iv), iv, sizeof(aad), aad);
 
-	ret = ARIA_Dec_Update(PADDING_BLOCK, cipher_text, cipher_len, plain_text, &plain_len, tag, tag_len);
-	if(ret != SUCCESS)
-	{
-		printf("aria gcm dec update failed\n");
-		return 1;
-	}
 	printf("Plain Text\n");
 	for(int i = 0; i < plain_len; i++)
-		printf("%#x ", plain_text[i]);
+		printf("%#x ", plain[i]);
 	printf("\n");
 
 #if 0
--- OpenSSLtool/src/aria.c
+++ OpenSSLtool/src/aria.c
@@ -5,12 +5,220 @@
 #include "../inc/defines.h"
 #include "../inc/err.h"
 
-EVP_CIPHER_CTX *evp_ctx_enc = NULL;
-EVP_CIPHER_CTX *evp_ctx_dec = NULL;
+U2 EncryptARIA(IN U1 *key, IN U1 padding_flag, IN U1 block_mode, IN U1 *plain_text, IN U4 plain_len, OUT U1 *cipher, OUT U4 *cipher_len, IN U1 req_tag_len, OUT U1 *tag, OUT U4 *tag_len, IN U2 iv_len, IN U1 *iv, IN U2 aad_len, IN U1 *aad)
+{
+	U2 ret = 0;
+	U4 outl = 0;
+	EVP_CIPHER_CTX *ctx = NULL;
+	U1 str_block_mode[12] = {0x00, };
+
+    U1 *cipher_buf = NULL;
+    U4 cipher_buf_len = 0;
+    int nBytesWritten = 0;
+
+	/* variable for TAG */
+	U4 tag_buf_len = 0;
+	U1 tag_buf[17] = {0, };
 
 
-U1 cipher_type[12];
+    U4 key_len = 16;
 
+    switch(block_mode)
+    {
+        case MODE_ECB :
+            sprintf(str_block_mode, "aria-%d-ecb", key_len*8);
+            break;
+        case MODE_CBC :
+            sprintf(str_block_mode, "aria-%d-cbc", key_len*8);
+            break;
+        case MODE_CTR :
+            sprintf(str_block_mode, "aria-%d-ctr", key_len*8);
+            break;
+        case MODE_GCM :
+            sprintf(str_block_mode, "aria-%d-gcm", key_len*8);
+            break;
+        default :
+            break;
+    }
+
+    const EVP_CIPHER *evp_cipher = EVP_get_cipherbyname(str_block_mode);
+    ctx = EVP_CIPHER_CTX_new();
+
+    if((evp_cipher == NULL) || (ctx == NULL))
+    {
+        printf("evp_cipher_enc OR evp_ctx_enc is NULL\n");
+        return 0xffff;
+    }
+
+    /* Encryption INIT */
+    ret = EVP_EncryptInit(ctx, evp_cipher, key, iv);
+    if(!ret)
+    {
+        printf("EVP_EncryptInit_ex ERROR\n");
+        return 0xffff;
+    }
+
+	if(block_mode == MODE_GCM)
+	{
+		/* Tag length should be in the range below */
+		if(req_tag_len > 16 || req_tag_len < 12){
+			printf("required tag length wrong\n");
+			return 0xffff;
+		}
+
+		ret = EVP_EncryptUpdate(ctx, NULL, (int *)&outl, (const unsigned char*)aad, aad_len);
+		outl = 0; // init 0 for update
+		if(!ret)
+		{
+			printf("Encrypt Init for GCM ERROR\n");
+			return 0xffff;
+		}
+	}
+
+    ret = EVP_CIPHER_CTX_set_padding(ctx, padding_flag);
+    if(!ret)
+    {
+        printf("EVP_CIPHER_CTX_set_padding ERROR\n");
+        return 0xffff;
+    }
+
+	/* Expand length of cipher buffer for padding */
+    cipher_buf_len = plain_len + EVP_CIPHER_CTX_block_size(ctx);
+
+    cipher_buf = (U1 *)malloc(cipher_buf_len);
+    if(cipher_buf == NULL)
+    {
+        printf("cipher buf malloc failed\n");
+        return 0xffff;
+    }
+
+    EVP_EncryptUpdate(ctx, &cipher_buf[outl], &nBytesWritten, plain_text, plain_len);
+    outl += nBytesWritten;
+
+    EVP_EncryptFinal(ctx, &cipher_buf[outl], &nBytesWritten);
+    outl += nBytesWritten;
+
+    memcpy(cipher, cipher_buf, outl);
+    *cipher_len = outl;
+
+	if (block_mode == MODE_GCM)
+	{
+		ret = EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_AEAD_GET_TAG, (int)req_tag_len, (unsigned char *)tag_buf);
+		if(!ret)
+		{
+			printf("EVP_CIPHER_CTX_ctrl ERROR\n");
+			return 0xffff;
+		}
+
+		tag_buf_len = strlen(tag_buf);
+		memcpy(tag, tag_buf, tag_buf_len);
+		*tag_len = tag_buf_len;
+	}
+
+    free(cipher_buf);
+	EVP_CIPHER_CTX_free(ctx);
+
+}
+
+
+U2 DecryptARIA(IN U1 *key, IN U1 padding_flag, IN U1 block_mode, IN U1 *cipher_text, IN U4 cipher_len,  OUT U1 *plain, OUT U4 *plain_len, IN U1 *tag, IN U1 tag_len, IN U2 iv_len, IN U1 *iv, IN U2 aad_len, IN U1 *aad)
+{
+	U2 ret = 0;
+	U4 outl = 0;
+	EVP_CIPHER_CTX *ctx = NULL;
+	U1 str_block_mode[12] = {0x00, };
+
+    int nBytesWritten = 0;
+
+    U4 plain_buf_len = 0;
+
+    U4 key_len = 16;
+
+	U1 plain_buf[128] = {0x00, };
+
+
+    switch(block_mode)
+    {
+        case MODE_ECB :
+            sprintf(str_block_mode, "aria-%d-ecb", key_len*8);
+            break;
+        case MODE_CBC :
+            sprintf(str_block_mode, "aria-%d-cbc", key_len*8);
+            break;
+        case MODE_CTR :
+            sprintf(str_block_mode, "aria-%d-ctr", key_len*8);
+            break;
+        case MODE_GCM :
+            sprintf(str_block_mode, "aria-%d-gcm", key_len*8);
+            break;
+        default :
+            break;
+    }
+
+    const EVP_CIPHER *evp_cipher = EVP_get_cipherbyname(str_block_mode);
+    ctx = EVP_CIPHER_CTX_new();
+
+    if((evp_cipher == NULL) || (ctx == NULL))
+    {
+        printf("evp_cipher_dec OR evp_ctx_dec is NULL\n");
+        return 0xffff;
+    }
+
+    /* Encryption INIT */
+    ret = EVP_DecryptInit(ctx, evp_cipher, key, iv);
+    if(!ret)
+    {
+        printf("EVP_DecryptInit_ex ERROR\n");
+        return 0xffff;
+    }
+
+	if(block_mode == MODE_GCM)
+    {
+        ret = EVP_DecryptUpdate(ctx, NULL, (int *)&outl, (const unsigned char*)aad, aad_len);
+        if(!ret)
+        {
+            printf("EVP_EncryptUpdate aad ERROR\n");
+            return 0xffff;
+        }
+		outl = 0; // Init 0 for updatte
+    }
+
+    ret = EVP_CIPHER_CTX_set_padding(ctx, padding_flag);
+    if(!ret)
+    {
+        printf("EVP_CIPHER_CTX_set_padding ERROR\n");
+        return 0xffff;
+    }
+
+    EVP_DecryptUpdate(ctx, &plain[outl], &nBytesWritten, cipher_text, cipher_len);
+    outl += nBytesWritten;
+
+	if(block_mode == MODE_GCM)
+	{
+		ret = EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_GCM_SET_TAG, (int)tag_len, (unsigned char *)tag);
+		if(!ret)
+		{
+			printf("dec SET TAG ERROR\n");
+			return 0xffff;
+		}
+	}
+
+
+	ret = EVP_DecryptFinal(ctx, &plain[outl], &nBytesWritten);
+	EVP_CIPHER_CTX_free(ctx);
+
+	if(ret > 0)
+	{
+		outl += nBytesWritten;
+		*plain_len = outl;
+
+		return SUCCESS;
+	}
+	else
+		return 0xFFFF;
+}
+
+#if 0
 U2 ARIA_Enc_Init(IN U1 *key, IN U1 block_mode, IN U2 iv_len, IN U1 *iv, IN U2 aad_len, IN U1 *aad)
 {
     U4 key_len = 16;
@@ -63,6 +271,7 @@ U2 ARIA_Enc_Init(IN U1 *key, IN U1 block_mode, IN U2 iv_len, IN U1 *iv, IN U2 aa
             return 0xffff;
         }
     }
+
     return 0x9000;
 }
 
@@ -108,7 +317,6 @@ U2 ARIA_Enc_Update(IN U1 padding_flag, IN U1 block_mode, IN U1 *plain_text, IN U
 
     memcpy(cipher, cipher_buf, outl);
     *cipher_len = outl;
-	DebugPrint();
 
 	if ( block_mode == MODE_GCM)
 	{
@@ -220,4 +428,4 @@ U2 ARIA_Dec_Update(IN U1 padding_flag, IN U1 *cipher_text, IN U4 cipher_len,  OU
 	else
 		return 0xFFFF;
 }
-
+#endif
