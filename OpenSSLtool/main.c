75764e20a7c8c67369b3032a603a00ae2e605104
GunwooYun <gunwoo87@gmail.com>
2022-12-09 19:02:02 +0900

    Add Save hashed ID sha256

--- OpenSSLtool/main.c
+++ OpenSSLtool/main.c
@@ -33,9 +33,11 @@ void init_data()
 {
 	FILE *fp_data = NULL;
 	U1 id_buf[32] = {0, };
+	U1 hashed_id[32] = {0, };
 	U1 pw_buf[32] = {0, };
 	U1 re_pw_buf[32] = {0, };
 	U1 salt[32] = {0, };
+	const U1 salt_len = 32;
 	U1 salted_pw[64] = {0, };
 	U1 hashed_pw[32] = {0, };
 	U4 writtenBytes = 0;
@@ -49,27 +51,34 @@ void init_data()
 	}
 
 	printf("Initialize Data\n");
-	printf("ID : ");
-	gets(id_buf);
+	printf("Create ID & Password\n");
+	printf("ID (5 ~ 32byte) : ");
+	fgets(id_buf, sizeof(id_buf), stdin);
+
+	printf("PW : ");
+	fgets(pw_buf, sizeof(pw_buf), stdin);
+
 	while(1)
 	{
-	printf("PW : ");
-	gets(pw_buf);
 	printf("Re type PW : ");
-	gets(re_pw_buf);
+	fgets(re_pw_buf, sizeof(re_pw_buf), stdin);
 
 	if(!strcmp(pw_buf, re_pw_buf)) break;
 	}
 
-	/* Generate salt */
-	GenCtrDRBG(sizeof(salt), salt);
+	/* Generate salt (len : 32byte) */
+	GenCtrDRBG(salt_len, salt);
 
+	/* | salt (16byte) | password (32byte) | salt (16byte) | */
 	memcpy(salted_pw, salt, sizeof(salt) / 2);
 	memcpy(salted_pw + (sizeof(salt) / 2), pw_buf, sizeof(pw_buf));
 	memcpy(salted_pw + (sizeof(salt) / 2) + sizeof(pw_buf), salt + sizeof(salt) / 2, sizeof(salt) / 2);
 
-	Sha256(salted_pw, sizeof(salted_pw), hashed_pw);
+	/* Execute SHA-256 for ID */
+	Sha256(id_buf, sizeof(id_buf), hashed_id);
 
+	/* Execute SHA-256 for pw */
+	Sha256(salted_pw, sizeof(salted_pw), hashed_pw);
 
 	fp_data = fopen("./.data", "wb");
 	if(fp_data == NULL)
@@ -77,7 +86,7 @@ void init_data()
 		printf("file open failure\n");
 		return;
 	}
-	writtenBytes = fwrite(id_buf, sizeof(U1) /* 1byte */, sizeof(id_buf), fp_data);
+	writtenBytes = fwrite(hashed_id, sizeof(U1) /* 1byte */, sizeof(hashed_id), fp_data);
 	assert(writtenBytes == 32);
 	writtenBytes = fwrite(hashed_pw, sizeof(U1) /* 1byte */, sizeof(hashed_pw), fp_data);
 	assert(writtenBytes == 32);
@@ -85,18 +94,18 @@ void init_data()
 	assert(writtenBytes == 32);
 
 	fclose(fp_data);
-
 }
 
 void log_in()
 {
-	U1 id[32] = {0, };
+	U1 saved_hashed_id[32] = {0, };
 	U1 saved_hashed_pw[32] = {0, };
 	U1 id_buf[32] = {0, };
 	U1 pw_buf[32] = {0, };
+	U1 input_hashed_id[32] = {0, };
+	U1 input_hashed_pw[32] = {0, };
 	U1 salt[32] = {0, };
 	U1 salted_pw[64] = {0, };
-	U1 input_hashed_pw[32] = {0, };
 	U4 readBytes = 0;
 
 	FILE *fp_data = NULL;
@@ -107,50 +116,58 @@ void log_in()
 		init_data();
 		return;
 	}
-	else
+
+	/* Fetch ID, PW, salt from data file */
+	readBytes = fread(saved_hashed_id, sizeof(U1), 32, fp_data); // fetch id
+	assert(readBytes == 32);
+	readBytes = fread(saved_hashed_pw, sizeof(U1), 32, fp_data); // fetch pw
+	assert(readBytes == 32);
+	readBytes = fread(salt, sizeof(U1), 32, fp_data); // fetch salt
+	assert(readBytes == 32);
+
+
+	printf("*** Log in ***\n");
+	while(1)
 	{
-		readBytes = fread(id, sizeof(U1), 32, fp_data); // fetch id
-		assert(readBytes == 32);
-		readBytes = fread(saved_hashed_pw, sizeof(U1), 32, fp_data); // fetch pw
-		assert(readBytes == 32);
-		readBytes = fread(salt, sizeof(U1), 32, fp_data); // fetch salt
-		assert(readBytes == 32);
-
-
-		printf("*** Log in ***\n");
+		memset(id_buf, 0, sizeof(id_buf));
 		printf("ID : ");
-		gets(id_buf);
-		while(1)
+		fgets(id_buf, sizeof(id_buf), stdin);
+
+		Sha256(id_buf, sizeof(id_buf), input_hashed_id);
+
+		if(arrcmp(input_hashed_id, saved_hashed_id, 32))
 		{
-			printf("PW : ");
-			gets(pw_buf);
-			memset(salted_pw, 0, sizeof(salted_pw));
-			memset(input_hashed_pw, 0, sizeof(input_hashed_pw));
-			memcpy(salted_pw, salt, sizeof(salt) / 2);
-			memcpy(salted_pw + (sizeof(salt) / 2), pw_buf, sizeof(pw_buf));
-			memcpy(salted_pw + (sizeof(salt) / 2) + sizeof(pw_buf), salt + sizeof(salt) / 2, sizeof(salt) / 2);
-
-			Sha256(salted_pw, sizeof(salted_pw), input_hashed_pw);
-
-			if(!arrcmp(input_hashed_pw, saved_hashed_pw, 32))
-			{
-				break;
-			}
-			else
-			{
-				printf("password not correct!\n");
-			}
+			continue;
 		}
 
-		/* Iterate hash 1000 times*/
-		for(int i = 0; i < 1000; i++)
-		{
-			Sha256(input_hashed_pw, sizeof(input_hashed_pw), input_hashed_pw);
-		}
-		memset(KEK, 0, sizeof(KEK));
-		Sha256(input_hashed_pw, sizeof(input_hashed_pw), KEK);
+		printf("PW : ");
+		fgets(pw_buf, sizeof(pw_buf), stdin);
+		memset(salted_pw, 0, sizeof(salted_pw));
+		memset(input_hashed_pw, 0, sizeof(input_hashed_pw));
+		memcpy(salted_pw, salt, sizeof(salt) / 2);
+		memcpy(salted_pw + (sizeof(salt) / 2), pw_buf, sizeof(pw_buf));
+		memcpy(salted_pw + (sizeof(salt) / 2) + sizeof(pw_buf), salt + sizeof(salt) / 2, sizeof(salt) / 2);
 
+		Sha256(salted_pw, sizeof(salted_pw), input_hashed_pw);
+
+		if(!arrcmp(input_hashed_pw, saved_hashed_pw, 32))
+		{
+			break;
+		}
+		else
+		{
+			printf("password not correct!\n");
+		}
 	}
+
+	/* Iterate hash 1000 times*/
+	for(int i = 0; i < 1000; i++)
+	{
+		Sha256(input_hashed_pw, sizeof(input_hashed_pw), input_hashed_pw);
+	}
+	memset(KEK, 0, sizeof(KEK));
+	Sha256(input_hashed_pw, sizeof(input_hashed_pw), KEK);
+
 	fclose(fp_data);
 	printf("Log-in Success\n");
 }
@@ -158,26 +175,7 @@ void log_in()
 
 int main(int argc, char **argv)
 {
-	//flag_need_init = 0;
-	//flag_logged_in = 0;
-
-	//init_data();
-	
-	//log_in();
-
-	//GenKeyAriaAes(0x00, 16);	
-	//GenKeyAriaAes(0x01, 24);	
-	//GenKeyAriaAes(0x02, 32);	
-
-	//testAria();
-	//testSha256();
-	//testHmac();
-
-	// testRSA_enc_dec();
-
-	testRSA_sign_verify();
-
-
-
+	init_data();
+	log_in();
 	return 0;
 }
