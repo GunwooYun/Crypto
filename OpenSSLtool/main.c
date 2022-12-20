f33e02d71e39991fbb5a229269e30eb47643e7cf
GunwooYun <gunwoo87@gmail.com>
2022-12-20 22:19:22 +0900

    1. Fix input length 2. change NL to 0 3. add get length of input

--- OpenSSLtool/main.c
+++ OpenSSLtool/main.c
@@ -1,6 +1,6 @@
 /*******************
 Author : Gunwoo Yun
-Date : 22.12.12
+Date : 22.12.17
 Crypto : ARIA HMAC-SHA256 SHA-256 GMAC RSA RSA_sign_verify ECDSA_sign_verify
 
 *******************/
@@ -32,6 +32,39 @@ int arrcmp(IN U1 *arr_a, IN U1 *arr_b, IN U4 len)
 	return 0;
 }
 
+int getStrLen(U1 *str, U4 strSize)
+{
+	U1 *pStr = str;
+	int cnt = 0;
+	while(cnt < strSize)
+	{
+		if(*pStr == '\n')
+		{
+			*pStr = 0;
+			break;
+		}
+		cnt++;
+		pStr++;
+		if(cnt >= strSize)
+		{
+			while(getchar() != '\n');
+			break;
+		}
+	}
+	/*
+	while((*(pStr++) != '\n'))
+	{
+		if(cnt >= (strSize-1))
+		{
+			while(getchar() != '\n');
+			break;
+		}
+		cnt++;
+	}
+	*/
+	return cnt;
+}
+
 void init_data()
 {
 	FILE *fp_data = NULL;
@@ -45,7 +78,10 @@ void init_data()
 	U1 hashed_pw[32] = {0, };
 	U4 writtenBytes = 0;
 
-	U1 id_buf_len = 0;
+	U4 id_buf_len = 0;
+	U4 pw_buf_len = 0;
+	char ch;
+	int ch_idx = 0;
 
 
 	fp_data = fopen("./.data", "rb");
@@ -60,27 +96,76 @@ void init_data()
 	do{
 		printf("ID (len : 5 ~ 20) : ");
 		fgets(id_buf, sizeof(id_buf), stdin);
+		//printf("id length : %d\n", chkStrLen(id_buf, 32));
 
 		/* fflush buffer of stdin*/
+		/*
 		if(strlen(id_buf) >= 31 && id_buf[30] != '\n')
 		{
 			while(getchar() != '\n');
 		}
-		id_buf_len = strlen(id_buf) - 1; // strlen read '\n'
+		*/
+		//id_buf_len = strlen(id_buf) - 1; // strlen read '\n'
+		id_buf_len = getStrLen(id_buf, 32);
+		//printf("id_buf_len : %d\n", id_buf_len);
 		if(id_buf_len >= 5 && id_buf_len <= 20) break; // 5 <= length of ID <= 20
 		printf("id length incorrect\n");
 	}while(1);
 
-	printf("PW : ");
-	fgets(pw_buf, sizeof(pw_buf), stdin);
+	do{
+		printf("PW (len : 5 ~ 30) : ");
+		fgets(pw_buf, sizeof(pw_buf), stdin);
+		/* fflush buffer of stdin*/
+			/*
+		if(strlen(pw_buf) >= 31 && pw_buf[30] != '\n')
+		{
+			while(getchar() != '\n');
+		}
+		*/
+		//pw_buf_len = strlen(pw_buf) - 1; // pw_buf + '\n'
+		pw_buf_len = getStrLen(pw_buf, 32);
+		//printf("pw_buf_len : %d\n", pw_buf_len);
+		if(pw_buf_len >= 5 && pw_buf_len <= 30) break; // 5 <= length of PW <= 30
+		printf("password length incorrect\n");
 
-	while(1)
+	}while(1);
+	printf("pw buf : ");
+	for(int i = 0; i < 32; i++)
 	{
-	printf("Re type PW : ");
-	fgets(re_pw_buf, sizeof(re_pw_buf), stdin);
-
-	if(!strcmp(pw_buf, re_pw_buf)) break;
+		printf("%x ", pw_buf[i]);
 	}
+	printf("\n");
+
+	do{
+		memset(re_pw_buf, 0, 32);
+		printf("Re type PW  (len : 5 ~ 30) : ");
+		fgets(re_pw_buf, sizeof(re_pw_buf), stdin);
+		/* fflush buffer of stdin*/
+			/*
+		if(strlen(pw_buf) >= 31 && pw_buf[30] != '\n')
+		{
+			while(getchar() != '\n');
+		}
+		*/
+		//pw_buf_len = strlen(pw_buf) - 1; // pw_buf + '\n'
+		pw_buf_len = getStrLen(re_pw_buf, 32);
+		printf("re pw_buf_len : %d\n", pw_buf_len);
+		if(pw_buf_len >= 5 && pw_buf_len <= 30)
+		{
+			if(!strcmp(pw_buf, re_pw_buf))
+			{
+				break;
+			}
+			else
+			{
+				printf("re password not same\n");
+			}
+		}
+		else
+		{
+			printf("password length incorrect\n");
+		}
+	}while(1);
 
 	/* Generate salt (len : 32byte) */
 	GenCtrDRBG(salt_len, salt);
@@ -110,6 +195,7 @@ void init_data()
 	writtenBytes = fwrite(hashed_pw, sizeof(U1) /* 1byte */, sizeof(hashed_pw), fp_data);
 	assert(writtenBytes == 32);
 
+
 	/* Write salt 32bytes */
 	writtenBytes = fwrite(salt, sizeof(U1) /* 1byte */, sizeof(salt), fp_data);
 	assert(writtenBytes == 32);
@@ -129,6 +215,8 @@ void log_in()
 	U1 salted_pw[64] = {0, };
 	U4 readBytes = 0;
 
+	U4 bufLen = 0;
+
 	bool loginLoop = true;
 	U1 flag_id_passed = 0;
 
@@ -146,6 +234,12 @@ void log_in()
 	assert(readBytes == 32);
 	readBytes = fread(saved_hashed_pw, sizeof(U1), 32, fp_data); // fetch pw
 	assert(readBytes == 32);
+	printf("saved hashed password : ");
+	for(int i = 0; i < 32; i++)
+	{
+		printf("%x ", saved_hashed_pw[i]);
+	}
+	printf("\n");
 	readBytes = fread(salt, sizeof(U1), 32, fp_data); // fetch salt
 	assert(readBytes == 32);
 
@@ -157,8 +251,15 @@ void log_in()
 		{
 			case 0 :
 				memset(id_buf, 0, sizeof(id_buf));
-				printf("ID : ");
+				printf("ID (len : 5 ~ 20) : ");
 				fgets(id_buf, sizeof(id_buf), stdin);
+
+				bufLen = getStrLen(id_buf, 32);
+				if(bufLen < 5 || bufLen > 20)
+				{
+					printf("id length incorrect\n");
+					break;
+				}
 
 				/* Get hashed input ID */
 				Sha256(id_buf, sizeof(id_buf), input_hashed_id);
@@ -171,8 +272,21 @@ void log_in()
 
 			case 1 :
 				memset(pw_buf, 0, sizeof(pw_buf));
-				printf("PW : ");
+				printf("PW (len : 5 ~ 30) : ");
 				fgets(pw_buf, sizeof(pw_buf), stdin);
+
+				bufLen = getStrLen(pw_buf, 32);
+				if(bufLen < 5 || bufLen > 20)
+				{
+					printf("pw length incorrect\n");
+					break;
+				}
+				printf("login pw buf : ");
+				for(int i = 0; i < 32; i++)
+				{
+					printf("%x ", pw_buf[i]);
+				}
+				printf("\n");
 				memset(salted_pw, 0, sizeof(salted_pw));
 				memset(input_hashed_pw, 0, sizeof(input_hashed_pw));
 
@@ -183,6 +297,13 @@ void log_in()
 
 				/* Get hashed input password */
 				Sha256(salted_pw, sizeof(salted_pw), input_hashed_pw);
+
+				printf("input hashed password : ");
+				for(int i = 0; i < 32; i++)
+				{
+					printf("%x ", input_hashed_pw[i]);
+				}
+				printf("\n");
 
 				if(!arrcmp(input_hashed_pw, saved_hashed_pw, 32))
 				{
